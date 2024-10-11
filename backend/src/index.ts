52bea397ce4aa0e63cdcfe6908a75421a52d4081
DEBASHIS GARAI <debashis1805@gmail.com>
2024-10-12 02:34:18 +0530

    prisma migrations and client generation done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,22 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const app = new Hono().basePath("/api/v1")
+
+// get the prisma client
+const prisma = new PrismaClient({
+  datasourceUrl: env.DATABASE_URL,
+}).$extends(withAccelerate());
+
+const app = new Hono().basePath("/api/v1");
 
 // endpoint for signup
-app.post('/signup', async (c) => {
-    const {fname, lname, username, pwd} = await c.req.json();
-    
-    return c.json({
-      message : "data fetched"
-    }) 
-})
+app.post("/signup", async (c) => {
+  const { fname, lname, username, pwd } = await c.req.json();
 
-export default app
+  return c.json({
+    message: "data fetched",
+  });
+});
+
+export default app;
